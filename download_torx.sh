9ddad0909e701edcbd9bbc6891eca3db1a40398b
nandan <nandan07@outlook.com>
2016-05-04 19:35:48 +0530

    modify information

--- download_torx.sh
+++ download_torx.sh
@@ -18,34 +18,35 @@ progress=0
 
 while [ $progress -lt 100 ]
 do 
-    wget -O $html_file $torx_link 2>/dev/null 
+    wget -q -O $html_file $torx_link
     progress=`cat $html_file|grep aria-valuenow |awk -F\" '{print$6}'`
     progress=`printf "%.0f" "$progress"`
     echo -ne "\r progress in server "$progress"%"
-    sleep 10
+    sleep 1
 done
-
-echo -ne "\rCompleted..!"
+echo ""
+echo "Completed..!"
 
 # extract links
 ./get_links.py $html_file > $links
 
 # Create Output folder
 folder=`cat $links |head -1|sed 's/ /_/g'`
-mkdir $out_loc""$folder
+mkdir -p $out_loc""$folder
 
 #save the links in tmp file
 sed '1d' $links > tmp
 
 i=1
+n=`wc -l $links`
 while read -r line
 do
     title=`echo "$line"|awk -F\| '{print$1}'|sed 's/ /_/g'`
     link=`echo "$line"|awk -F\| '{print$2}'|sed 's/amp;//g'`
     out_file=$out_loc""$folder"/"$title
-    echo "$i"
+    echo "Downloading $i out of $n"
     i=$(($i+1))
-    wget -O $out_file $link
+    wget -q --show-progress -O $out_file $link
 done<tmp
 rm tmp $links
 echo "Done..!"
